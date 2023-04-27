f649011a59e931be6aa1a1d8d01daa027e41b02e
Andrei Christian <andreichristiannetoiu@gmail.com>
2023-04-27 21:54:18 +0200

    hello

--- frontend_admin/src/app/facility/facility-category/facility-category.component.ts
+++ frontend_admin/src/app/facility/facility-category/facility-category.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { Observable } from 'rxjs';
+import { HttpService } from 'src/app/services/http.service';
 
 @Component({
   selector: 'app-facility-category',
   templateUrl: './facility-category.component.html',
-  styleUrls: ['./facility-category.component.scss']
+  styleUrls: ['./facility-category.component.scss'],
 })
-export class FacilityCategoryComponent {
+export class FacilityCategoryComponent implements OnInit {
+  displayedColumns: string[] = ['name', 'subcategories', 'edit', 'delete'];
+  dataSource = [];
 
+  isLoading: boolean = true;
+
+  constructor(private httpService: HttpService) {}
+
+  ngOnInit(): void {
+    this.httpService.loading$.subscribe(
+      (data: boolean) => (this.isLoading = data)
+    );
+    this.httpService.getList('facility_categories');
+    this.httpService.data$.subscribe((data) => (this.dataSource = data));
+
+  }
 }
--- frontend_admin/src/app/facility/facility-subcategory/facility-subcategory.component.ts
+++ frontend_admin/src/app/facility/facility-subcategory/facility-subcategory.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { HttpService } from 'src/app/services/http.service';
 
 @Component({
   selector: 'app-facility-subcategory',
   templateUrl: './facility-subcategory.component.html',
-  styleUrls: ['./facility-subcategory.component.scss']
+  styleUrls: ['./facility-subcategory.component.scss'],
 })
 export class FacilitySubcategoryComponent {
+  displayedColumns: string[] = ['name', 'items', 'edit', 'delete'];
+  dataSource = [];
 
+  isLoading: boolean = true;
+
+  constructor(private httpService: HttpService) {}
+
+  ngOnInit(): void {
+    this.httpService.loading$.subscribe(
+      (data: boolean) => (this.isLoading = data)
+    );
+    this.httpService.getList('facility_subcategories');
+    this.httpService.data$.subscribe((data) => {
+      (this.dataSource = data), console.log(data);
+    });
+  }
 }

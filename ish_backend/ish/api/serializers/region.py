7e9beeccc0fef205df4a878116ee6935537805ad
Andrei Christian <andreichristiannetoiu@gmail.com>
2023-04-27 10:50:22 +0200

    tweaking serializers for regions and build the UI

--- ish_backend/ish/api/serializers/region.py
+++ ish_backend/ish/api/serializers/region.py
@@ -2,6 +2,10 @@ from rest_framework import serializers
 from api.models import Region
 
 class RegionSerializer(serializers.ModelSerializer):
+
+    properties = serializers.StringRelatedField(many=True)
+    staff_members = serializers.StringRelatedField(many=True)
+    managers = serializers.StringRelatedField(many=True)
     class Meta:
         model = Region
-        fields = ['id', 'name', 'description']
+        fields = ['id', 'name', 'description','properties','staff_members','managers']

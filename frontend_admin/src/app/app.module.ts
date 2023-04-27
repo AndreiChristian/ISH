3b804e4cf412eb0a2befab18185d493f24250e20
Andrei Christian <andreichristiannetoiu@gmail.com>
2023-04-27 10:43:59 +0200

    added dashboard

--- frontend_admin/src/app/app.module.ts
+++ frontend_admin/src/app/app.module.ts
@@ -15,15 +15,38 @@ import { FacilitySubcategoryComponent } from './facility/facility-subcategory/fa
 import { RegionComponent } from './region/region.component';
 import { HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './home/home.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { LayoutModule } from '@angular/cdk/layout';
 
 @NgModule({
-  declarations: [AppComponent, NavigationComponent, NavigationMenuFacilitiesComponent, NavigationMenuReservationsComponent, FacilityComponent, FacilityItemComponent, FacilityCategoryComponent, FacilitySubcategoryComponent, RegionComponent, HomeComponent],
+  declarations: [
+    AppComponent,
+    NavigationComponent,
+    NavigationMenuFacilitiesComponent,
+    NavigationMenuReservationsComponent,
+    FacilityComponent,
+    FacilityItemComponent,
+    FacilityCategoryComponent,
+    FacilitySubcategoryComponent,
+    RegionComponent,
+    HomeComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
-    HttpClientModule
+    HttpClientModule,
+    MatGridListModule,
+    MatCardModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule,
+    LayoutModule,
   ],
   providers: [MaterialModule],
   bootstrap: [AppComponent],
--- frontend_admin/src/app/home/home.component.ts
+++ frontend_admin/src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +8,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  /** Based on the screen size, switch from standard to one column per row */
+  cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
+    map(({ matches }) => {
+      if (matches) {
+        return [
+          { title: 'Card 1', cols: 1, rows: 1 },
+          { title: 'Card 2', cols: 1, rows: 1 },
+          { title: 'Card 3', cols: 1, rows: 1 },
+          { title: 'Card 4', cols: 1, rows: 1 }
+        ];
+      }
 
+      return [
+        { title: 'Card 1', cols: 2, rows: 1 },
+        { title: 'Card 2', cols: 1, rows: 1 },
+        { title: 'Card 3', cols: 1, rows: 2 },
+        { title: 'Card 4', cols: 1, rows: 1 }
+      ];
+    })
+  );
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
 }

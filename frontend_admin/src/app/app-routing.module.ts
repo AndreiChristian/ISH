167a692bd80007191c6280bc8f60ea3bad6f6ea3
Andrei Christian <andreichristiannetoiu@gmail.com>
2023-04-27 08:35:09 +0200

    onwards to http

--- frontend_admin/src/app/app-routing.module.ts
+++ frontend_admin/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegionComponent } from './region/region.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"regions",component:RegionComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

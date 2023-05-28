c12ed332ca4858f7e27187b8b8e80fb4c41a1f4d
Andrei Christian <andreichristiannetoiu@gmail.com>
2023-05-28 14:30:32 +0200

    setup for reservations

--- frontend_client/src/app/reservations/reservations-facilities/reservations-facilities-item/reservations-facilities-item.component.ts
+++ frontend_client/src/app/reservations/reservations-facilities/reservations-facilities-item/reservations-facilities-item.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-reservations-facilities-item',
   templateUrl: './reservations-facilities-item.component.html',
-  styleUrls: ['./reservations-facilities-item.component.scss']
+  styleUrls: ['./reservations-facilities-item.component.scss'],
 })
-export class ReservationsFacilitiesItemComponent {
+export class ReservationsFacilitiesItemComponent implements OnInit, OnDestroy {
+  @Input() subcategory_id: number;
 
+  facilities$: Observable<any>;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.facilities$ = this.http.get(
+      `http://localhost:8080/api/facilities/subcategory/${this.subcategory_id}`
+    );
+  }
+
+  ngOnDestroy(): void {}
 }
--- frontend_client/src/app/reservations/reservations-facilities/reservations-facilities-subcategory/reservations-facilities-subcategory.component.ts
+++ frontend_client/src/app/reservations/reservations-facilities/reservations-facilities-subcategory/reservations-facilities-subcategory.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, Input, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-reservations-facilities-subcategory',
   templateUrl: './reservations-facilities-subcategory.component.html',
-  styleUrls: ['./reservations-facilities-subcategory.component.scss']
+  styleUrls: ['./reservations-facilities-subcategory.component.scss'],
 })
-export class ReservationsFacilitiesSubcategoryComponent {
+export class ReservationsFacilitiesSubcategoryComponent implements OnInit {
+  constructor(private http: HttpClient) {}
 
+  @Input() category_id: number;
+
+  facilitiesSubcategories$: Observable<any>;
+
+  ngOnInit(): void {
+    this.facilitiesSubcategories$ = this.http.get(
+      `http://localhost:8080/api/facilities_subcategory/category/${this.category_id}`
+    );
+  }
 }

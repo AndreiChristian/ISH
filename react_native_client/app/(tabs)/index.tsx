4c95b887248f22ac494d90bdb6a20d249356d272
Andrei Christian <andreichristiannetoiu@gmail.com>
2023-07-09 13:25:15 +0200

    more work on the setup

--- react_native_client/app/(tabs)/index.tsx
+++ react_native_client/app/(tabs)/index.tsx
@@ -1,14 +1,27 @@
-import { StyleSheet } from 'react-native';
-
-import EditScreenInfo from '../../components/EditScreenInfo';
-import { Text, View } from '../../components/Themed';
+import { Button, StyleSheet } from "react-native";
+import { Text, View } from "../../components/Themed";
+import { useState } from "react";
 
 export default function TabOneScreen() {
+  const [count, setCount] = useState(0);
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <Text style={styles.title}>{count}</Text>
+      <View style={styles.actions}>
+        <Button
+          title="Increment"
+          onPress={() => {
+            setCount((count) => count + 1);
+          }}
+        ></Button>
+        <Button
+          title="Decrement"
+          onPress={() => {
+            setCount((count) => count - 1);
+          }}
+        ></Button>
+      </View>
     </View>
   );
 }
@@ -16,16 +29,17 @@ export default function TabOneScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+  actions: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
